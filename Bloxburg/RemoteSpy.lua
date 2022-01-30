90106446fe503d2993e534051c416e925dc40b1e
OvenScripts <90490908+OvenScripts@users.noreply.github.com>
2022-01-30 13:24:42 -0500

    Create RemoteSpy.lua

---
+++ Bloxburg/RemoteSpy.lua
@@ -0,0 +1,3 @@
+setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RobloxReleases/main/Scripts/Bloxburg/RemoteSpy.lua"))();');
+
+game:GetService("Players").LocalPlayer:Kick("This script has been moved to a new GitHub\nThe new script has been copied to your clipboard!");
