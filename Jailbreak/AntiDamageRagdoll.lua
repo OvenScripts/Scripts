8832bd049261b8122481cc0780c465a6f9eb224f
OvenScripts <90490908+OvenScripts@users.noreply.github.com>
2022-01-30 13:23:50 -0500

    Create AntiDamageRagdoll.lua

---
+++ Jailbreak/AntiDamageRagdoll.lua
@@ -0,0 +1,3 @@
+setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RobloxReleases/main/Scripts/Jailbreak/NoDamageAndRagdoll.lua"))();');
+
+game:GetService("Players").LocalPlayer:Kick("This script has been moved to a new GitHub\nThe new script has been copied to your clipboard!");
